cfc1b3f315413ff6ad5a1e0177bc03a2a44bd19c
Christian <sefluntok@gmail.com>
2023-04-19 20:55:50 +1000

    Deleted old const data.

---
+++ src/data/consts.ts
@@ -0,0 +1,11 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Astro Lane - Avenue Labs";
+export const SITE_DESCRIPTION =
+    "Astro Lane - A personal portfolio landing template for developers and designers. Made by Avenue Labs.";
+export const SITE_KEYWORDS =
+    "Personal portfolio, landing page, page template, developer portfoliom designer portfolio";
+export const SITE_URL = "https:/www.lane.avenuelabs.co";
+export const SITE_IMAGE_PREVIEW_URL = "images/lane.png";
+export const TWITTER_HANDLE = "@your_handle";
