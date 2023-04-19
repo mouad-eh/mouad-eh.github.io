bfe95616fec7b313cef529f61867f6029d05c605
Christian <sefluntok@gmail.com>
2023-04-19 10:16:58 +1000

    Added new packages and set up package.json content nicely.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
+import { defineConfig } from "astro/config";
+import { remarkReadingTime } from "./remark-reading-time";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+    site: "https://example.com",
+    markdown: {
+        syntaxHighlight: "prism",
+        remarkPlugins: [remarkReadingTime]
+    },
+    vite: {
+        ssr: {
+            external: ["svgo"]
+        }
+    },
+    integrations: [mdx(), sitemap(), tailwind()]
 });
