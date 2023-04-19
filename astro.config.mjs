43ed78d30a96f91e9b1da3ce2e610424fe99723c
Christian <sefluntok@gmail.com>
2023-04-19 19:58:30 +1000

    Added astro image.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,19 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://example.com",
-    markdown: {
-        syntaxHighlight: "prism",
-        remarkPlugins: [remarkReadingTime]
-    },
-    vite: {
-        ssr: {
-            external: ["svgo"]
-        }
-    },
-    integrations: [mdx(), sitemap(), tailwind()]
+  site: "https://example.com",
+  markdown: {
+    syntaxHighlight: "prism",
+    remarkPlugins: [remarkReadingTime]
+  },
+  vite: {
+    ssr: {
+      external: ["svgo"]
+    }
+  },
+  integrations: [mdx(), sitemap(), tailwind(), image()]
 });
