3d72ad62d45db15e5cd89badf7fdffdb9857e19c
Christian <sefluntok@gmail.com>
2023-04-21 06:38:58 +1000

    Setup config, types and env data.

---
+++ src/config.ts
@@ -0,0 +1,14 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+import type { Site } from "./types";
+
+export const SITE: Site = {
+    siteUrl: "https:/www.lane.avenuelabs.co",
+    author: "Christian Luntok - Avenue Labs",
+    desc: "A personal portfolio landing template for developers and designers. Made by Avenue Labs.",
+    title: "Astro Lane - Avenue Labs",
+    ogImage: "images/lane.png",
+    keywords:
+        "Personal portfolio, landing page, page template, developer portfoliom designer portfolio",
+    postPerPage: 3
+};
---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export type Site = {
+    siteUrl: string;
+    author: string;
+    desc: string;
+    title: string;
+    ogImage: string;
+    keywords: string;
+    postPerPage: number;
+};
