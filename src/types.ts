9362da9b7d3d195df8d221d8e1319a9785906387
Christian <sefluntok@gmail.com>
2023-04-22 10:07:56 +1000

    Added social media types and default config file.

--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,28 @@ export type Site = {
     keywords: string;
     postPerPage: number;
 };
+
+export type SocialMediaObjects = {
+    name: SocialMediaTypes;
+    href: string;
+    icon?: string;
+    active: boolean;
+    title: string;
+}[];
+
+export type SocialIcons = {
+    [social in SocialMediaTypes]: string;
+};
+
+export type SocialMediaTypes =
+    | "Github"
+    | "Facebook"
+    | "Instagram"
+    | "LinkedIn"
+    | "Mail"
+    | "Twitter"
+    | "YouTube"
+    | "WhatsApp"
+    | "Snapchat"
+    | "CodePen"
+    | "Discord";
